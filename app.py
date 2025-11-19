d99760bde7694fd15261fb3108ed540965ad29ee
connorfeth <connorfeth@users.noreply.github.com>
2025-11-18 21:10:39 -0500

    began implementation for login

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, redirect, url_for
+from flask import Flask, render_template, redirect, url_for, request, flash
 from db import get_db
 
 app = Flask(__name__)
@@ -7,8 +7,18 @@ app = Flask(__name__)
 def index():
     return redirect(url_for("login"))
 
-@app.route("/login")
+@app.route("/login", methods=["GET", "POST"])
 def login():
+    if request.method == "POST":
+        username = request.form["username"].strip()
+        password = request.form["password"].strip()
+
+        if not username or not password:
+            flash("Username and password cannot be empty.")
+            return render_template("login.html")
+        else:
+            return redirect(url_for("employees"))
+        
     return render_template("login.html")
 
 @app.route("/employees")
@@ -20,3 +30,6 @@ def employees():
             """)
             employees_list = cur.fetchall()
     return render_template("employees.html", employees=employees_list)
+
+if __name__ == "__main__":
+    app.run(debug=True)
