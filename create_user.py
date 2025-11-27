51afe58081d48f8caead3044625bb92a4a7c2d0d
CalebFriesen <calebjofriesen@gmail.com>
2025-11-26 23:37:03 -0500

    add a viewer

--- create_user.py
+++ create_user.py
@@ -1,10 +1,17 @@
 import os
+import sys
 import psycopg
 from werkzeug.security import generate_password_hash
 
+
+if len(sys.argv) != 2:
+    print("Usage: python script.py <role>")
+    sys.exit(1)
+
+
 database_url = os.environ.get("DATABASE_URL")
 
-username = "admin"
+username = sys.argv[1]
 password = "password"
 
 pw_hash = generate_password_hash(password)
@@ -13,7 +20,7 @@ with psycopg.connect(database_url) as conn:
     with conn.cursor() as cur:
         cur.execute(
             "INSERT INTO app_user (username, password_hash, role) VALUES (%s, %s, %s) ON CONFLICT (username) DO NOTHING",
-            (username, pw_hash, "admin")
+            (username, pw_hash, sys.argv[1])
         )
         conn.commit()
 print("User created (or already exists).")
